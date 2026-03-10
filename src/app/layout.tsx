4f63dd38637c2b9a5103d7c027e218caffb80b59
abdullahdhillu <Abdullahdhillu99@gmail.com>
2026-03-10 12:21:30 +0500

    test commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter, Geist } from "next/font/google";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/providers/theme-provider";
 import "./globals.css";
-import { cn } from "@/lib/utils";
-
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,12 +14,23 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en" className={cn("font-sans", geist.variable)}>
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className="font-sans" suppressHydrationWarning>
+        <body suppressHydrationWarning className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            disableTransitionOnChange
+          >
+            {children}
+            <Toaster />
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ src/components/global/loader/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+import { Spinner } from "./spinner";
+
+type Props = {
+  state: boolean;
+  className?: string;
+  children: React.ReactNode;
+  color?: string;
+};
+
+const Loader = ({ state, className, children, color }: Props) => {
+  return state ? (
+    <div className={cn(className)}>
+      <Spinner color={color} />
+    </div>
+  ) : (
+    children
+  );
+};
+
+export default Loader;
