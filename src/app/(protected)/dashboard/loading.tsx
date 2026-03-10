4a260e144ff9844884c3fdb361f2010ef8be442d
abdullahdhillu <Abdullahdhillu99@gmail.com>
2026-03-10 12:24:57 +0500

    Testing commit

--- src/app/(protected)/dashboard/loading.tsx
+++ src/app/(protected)/dashboard/loading.tsx
@@ -6,7 +6,7 @@ type Props = {};
 const Loading = (props: Props) => {
   return (
     <div className="h-screen flex justify-center items-center">
-      <Loader state>...Loading</Loader>
+      <Loader state>...Loadinggg</Loader>
     </div>
   );
 };
