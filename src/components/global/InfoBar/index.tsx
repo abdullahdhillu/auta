c1fb41ed8b329f2879c7b8204b5ec8cc4edeefeb
abdullahdhillu <Abdullahdhillu99@gmail.com>
2026-03-12 13:11:12 +0500

    Finished creating automation components and deeper automation triggers set up the backend server

--- src/components/global/InfoBar/index.tsx
+++ src/components/global/InfoBar/index.tsx
@@ -13,7 +13,7 @@ import { LogoSmall } from "@/svgs/logo-small";
 import CreateAutomation from "../create-automation";
 import Search from "./search";
 import { Notifications } from "./notifications";
-import MainBreadCrumb from "../main-bread-crumb";
+import MainBreadCrumb from "../bread-crumbs/main-bread-crumb";
 import Image from "next/image";
 
 type Props = {
---
+++ src/components/global/activate-automation-button/index.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+import Loader from "../loader";
+import {
+  ActivitySquareIcon,
+  GitPullRequestArrowIcon,
+  WavesIcon,
+} from "lucide-react";
+
+type Props = {};
+
+const ActivateAutomationButton = (props: Props) => {
+  return (
+    <Button className="lg:px-10 bg-gradient-to-br hover:opacity-80 text-white rounded-full from-[#3352CC] font-medium to-[#1C2D70] ml-4">
+      <Loader state={false}>
+        <GitPullRequestArrowIcon />
+        <p className="lg:inline hidden">Activate</p>
+      </Loader>
+    </Button>
+  );
+};
+
+export default ActivateAutomationButton;
