e8d82f082b98969372a907861904cfed55f5b744
abdullahdhillu <Abdullahdhillu99@gmail.com>
2026-03-20 15:55:45 +0500

    Integrated Stripe

--- src/components/global/billing/index.tsx
+++ src/components/global/billing/index.tsx
@@ -1,13 +1,24 @@
+"use client";
 import React from "react";
 import PaymentCard from "./payment-card";
+import { useQueryUser } from "@/hooks/user-queries";
+import { SubscriptionPlan } from "../subscription-plan";
+import UpgradeCrad from "../sidebar/upgrade";
 
 type Props = {};
 
 const Billing = (props: Props) => {
+  const { data } = useQueryUser();
+
   return (
     <div className="flex lg:flex-row flex-col gap-5 w-full lg:w-10/12 xl:w-8/12 container">
-      <PaymentCard current={"FREE"} label="FREE" />
-      <PaymentCard current={"FREE"} label="PRO" />
+      <PaymentCard current={data?.data?.subscription?.plan!} label="PRO" />
+      <PaymentCard current={data?.data?.subscription?.plan!} label="FREE" />
+      <SubscriptionPlan type="FREE">
+        <div className="flex-1 flex flex-col justify-end">
+          <UpgradeCrad />
+        </div>
+      </SubscriptionPlan>
     </div>
   );
 };
--- src/components/global/payment-button/index.tsx
+++ src/components/global/payment-button/index.tsx
@@ -2,11 +2,18 @@
 import { Button } from "@/components/ui/button";
 import { useSubscription } from "@/hooks/use-subscription";
 import React from "react";
+import Loader from "../loader";
+import { CreditCardIcon, Loader2 } from "lucide-react";
 
 const PaymentButton = () => {
   const { onSubscribe, isProcessing } = useSubscription();
   return (
-    <Button className="bg-gradient-to-br text-white rounded-full from-[#9685DB] via-[#9434E6] font-bold to-[#CC3BD4]">
+    <Button
+      disabled={isProcessing}
+      onClick={onSubscribe}
+      className="bg-gradient-to-br text-white rounded-full from-[#9685DB] via-[#9434E6] font-bold to-[#CC3BD4]"
+    >
+      {isProcessing ? <Loader2 className="animate-spin" /> : <CreditCardIcon />}
       Upgrade
     </Button>
   );
